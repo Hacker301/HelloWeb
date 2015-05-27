adc32eab1ab2f477ce8785dc15270befb0f27974
cuibty <cuibty@qq.com>
2015-05-28 03:10:45 +0800

    初始化提交

---
+++ HelloWorld.Entity/src/main/java/com/huihet/entity/Diamond.java
@@ -0,0 +1,23 @@
+package com.huihet.entity;
+
+public class Diamond {
+    private int _Id;
+    private String _Name;
+
+    public String get_Name() {
+        return _Name;
+    }
+
+    public void set_Name(String _Name) {
+        this._Name = _Name;
+    }
+
+    public int get_Id() {
+        return _Id;
+    }
+
+    public void set_Id(int _Id) {
+        this._Id = _Id;
+    }
+
+}
